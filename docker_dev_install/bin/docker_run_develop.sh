f81b1b0e5c09f96b8a58a7c1cf7f563aeb3941e9
Jinagxumin <cjiangxumin@gmail.com>
2022-02-11 15:46:02 +0800

    updatge ....

--- docker_dev_install/bin/docker_run_develop.sh
+++ docker_dev_install/bin/docker_run_develop.sh
@@ -170,7 +170,8 @@ mkdir -p $SHARED_HOST_DIR
 #IMAGE=$IMAGE_NAME:$TAG_PREFIX-$ROS_DISTRO$SUFFIX
 # IMAGE=registry.cn-hangzhou.aliyuncs.com/cudagl/ros2:galactic
 # IMAGE=cudagl:autowareArchitectureProposal_local
-IMAGE=cudagl:autowareArchitectureProposal_local_base
+# IMAGE=cudagl:autowareArchitectureProposal_local_base
+IMAGE=192.168.2.100:8086/pm-autopilot/cudagl:autowareArchitectureProposal_local_v1
 
 echo "Launching $IMAGE"
 
